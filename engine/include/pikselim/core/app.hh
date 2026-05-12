64ffd622c37a5a90e3fbde1df87283fbc3d50d98
v4m3rrr <igorurbanowicz@gmail.com>
2026-05-12 20:14:26 +0200

    Set up documentation

--- engine/include/pikselim/core/app.hh
+++ engine/include/pikselim/core/app.hh
@@ -1,6 +1,20 @@
+/* 
+ * Copyright (c) 2026 Pikselim Project
+ * Author: v4m3rrr
+ * 
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for details.
+ */
 #pragma once
 
 namespace pikselim::core{
+  /**
+   * @file app.hh
+   * @brief 
+   * @author v4m3rrr
+   * 
+   * @date 2026-05-12
+   */
   class App
   {
   public:
--- engine/src/core/app.cc
+++ engine/src/core/app.cc
@@ -1,3 +1,10 @@
+/* 
+ * Copyright (c) 2026 Pikselim Project
+ * Author: v4m3rrr
+ * 
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for details.
+ */
 #include "pikselim/core/app.hh"
 
 namespace pikselim::core{
