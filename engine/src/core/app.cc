303b7ab28f15338402a2e55e9ee85e49ffadf8e1
v4m3rrr <igorurbanowicz@gmail.com>
2026-05-14 17:29:18 +0200

    Add spdlog

--- engine/src/core/app.cc
+++ engine/src/core/app.cc
@@ -13,6 +13,7 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
+#include <spdlog/spdlog.h>
 
 #include <iostream>
 
@@ -58,6 +59,15 @@ int App::test() {
 
   std::cout << "Result: (" << result.x << ", " << result.y << ", " << result.z
             << ")\n";
+  spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR,
+               SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical(
+      "Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+  spdlog::info("Support for floats {:03.2f}", 1.23456);
+  spdlog::info("Positional args are {1} {0}..", "too", "supported");
+  spdlog::info("{:>8} aligned, {:<8} aligned", "right", "left");
 
   glfwDestroyWindow(window);
   glfwTerminate();
